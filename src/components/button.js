2029b0fdfeecdc7667cd39c85eaae1a365ced8b1
Paul <pwittschen@gmail.com>
2017-12-12 19:34:19 -0500

    some syntax clean up

--- src/components/button.js
+++ src/components/button.js
@@ -2,15 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Button extends React.Component {
-    render() {
-        return (
-            <button className='btn' onClick={this.props.onClick}>{this.props.text}</button>
-        )
-    }
+  render() {
+    return (
+      <button 
+        className='btn' 
+        onClick={this.props.onClick}>
+        {this.props.text}
+      </button>
+    )
+  }
 }
 Button.propTypes = {
-    onClick: PropTypes.func,
-    text: PropTypes.string
+  onClick: PropTypes.func,
+  text: PropTypes.string
 }
 
 export default Button;
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,32 +1,23 @@
 import { TOGGLE_CELL, BOARD_STEP, RESET_BOARD, RANDOMIZE_BOARD } from '../constants';
 
 const actions = {
-
-    toggleCell: (row, col, updateTo) => {
-        return {
-            type: TOGGLE_CELL,
-            row,
-            col,
-            updateTo
-        }
-    },
-    boardStep: () => {
-        return {
-            type: BOARD_STEP
-        }
-    },
-    resetBoard: (size, cellValue) => {
-        return {
-            type: RESET_BOARD,
-            size,
-            cellValue
-        }
-    },
-    randomizeBoard: () => {
-        return {
-            type: RANDOMIZE_BOARD
-        }
-    }
+	toggleCell: (row, col, updateTo) => ({
+		type: TOGGLE_CELL,
+		row,
+		col,
+		updateTo
+	}),
+	boardStep: () => ({
+		type: BOARD_STEP
+	}),
+	resetBoard: (size, cellValue) => ({
+		type: RESET_BOARD,
+		size,
+		cellValue
+	}),
+	randomizeBoard: () => ({
+		type: RANDOMIZE_BOARD
+	})
 };
 
 export default actions;
